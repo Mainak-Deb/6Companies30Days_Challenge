92124c22191059df08dffb71500a7238a0528c16
Mainak <debmainak37@gmail.com>
2022-01-24 13:39:08 +0530

    az8

---
+++ Amazon/AZ8_Nth_stair.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+import math
+
+class Solution:
+    #Function to count number of ways to reach the nth stair 
+    #when order does not matter.
+    def countWays(self,m):
+        return m-(math.ceil(m/2))+1
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        m = int(input())
+        ob=Solution()
+        print(ob.countWays(m))
+
+# } Driver Code Ends
